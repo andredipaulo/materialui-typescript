d1bf021a4c14304d8dbfc8cb06f76aacde9e6cd4
André Di Paulo <andredipaulo@gmail.com>
2023-12-05 16:39:29 -0300

    feat: instalação do material-ui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
+import { Button } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 export default function AppRoutes(){
     return (
 
         <Routes>
-            <Route path="/" element={<p>Pagina Incial</p>}/>
+            <Route path="/" element={<Button>Teste</Button>}/>
 
             <Route path="*" element={<Navigate to={"/"} />}/>
         </Routes>
